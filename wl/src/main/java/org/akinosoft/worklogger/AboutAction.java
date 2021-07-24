4dddf3fca7f09c42b9510c0195d51d0345c22a8b
idaho06 <cesar.idaho@gmail.com>
2021-07-24 16:32:32 +0200

    Lambdas are beautiful

--- wl/src/main/java/org/akinosoft/worklogger/AboutAction.java
+++ wl/src/main/java/org/akinosoft/worklogger/AboutAction.java
@@ -3,7 +3,6 @@ package org.akinosoft.worklogger;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.io.Serial;
 
@@ -59,12 +58,13 @@ public class AboutAction extends AbstractAction {
             aboutPanel.add(okButton, BorderLayout.SOUTH);
             getContentPane().add(aboutPanel, BorderLayout.CENTER);
 
-            okButton.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent evt) {
-                    setVisible(false);
-                }
-            });
+            okButton.addActionListener(evt -> setVisible(false));
 
+            this.getRootPane().registerKeyboardAction(
+                    evt -> setVisible(false),
+                    KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+                    JComponent.WHEN_IN_FOCUSED_WINDOW);
+            /*
             this.getRootPane().registerKeyboardAction(
                     new ActionListener() {
                         public void actionPerformed(ActionEvent evt) {
@@ -73,6 +73,8 @@ public class AboutAction extends AbstractAction {
                     },
                     KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
                     JComponent.WHEN_IN_FOCUSED_WINDOW);
+             */
+
 
             this.pack();
             this.setResizable(false);
